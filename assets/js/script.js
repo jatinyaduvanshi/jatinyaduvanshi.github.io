492e03c588e29274ba699c352ac525b278eacdc7
Jatin Yaduvanshi <cehjatinkumar@gmail.com>
2024-08-26 10:44:20 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 // sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
